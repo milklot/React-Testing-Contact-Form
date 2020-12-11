4ad2a64098f4e478c95c967532e6c97413d25316
milklot <vbiostflex@gmail.com>
2020-12-10 20:24:54 -0500

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -11,19 +11,24 @@ test('renders ContactForm without issues', () => {
 test("fill out and submit the form by user", async () => {
 	render(<ContactForm/>);
 
+
+	//assign our fields and submit button to the variables
 	const fName = screen.getByPlaceholderText(/Edd/i);
 	const lName = screen.getByPlaceholderText(/Burke/i);
 	const email = screen.getByPlaceholderText(/test@test.com/i);
 	const submitBtn = screen.getByRole('button');
 
+	//add some input in our fields
 	fireEvent.change(fName, {target: {value: "Eli"}});
 	fireEvent.change(lName, {target: {value: "Ted"}});
 	fireEvent.change(email, {target: {value: "testreact@test.com"}});
 
+	// test input values of fields
 	expect(fName.value).toBe("Eli");
 	expect(lName.value).toBe("Ted");
 	expect(email.value).toBe("testreact@test.com");
 
+	//click submit button
 	userEvent.click(submitBtn);
 
 	expect(fName).toBeInTheDocument();
